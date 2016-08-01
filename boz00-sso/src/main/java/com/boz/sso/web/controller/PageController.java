bbdace5934db6a163d45e8805488a5120e120db5
Elliotbo <wavying2012@163.com>
2016-08-01 09:34:07 +0800

    123

---
+++ boz00-sso/src/main/java/com/boz/sso/web/controller/PageController.java
@@ -0,0 +1,29 @@
+/*
+ * Creation : 2016年8月1日
+ */
+/**
+ * 
+ */
+package com.boz.sso.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * PageController.
+ * 
+ * @author E470756
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/page/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/page/register")
+    public String register() {
+        return "register";
+    }
+}
---
+++ boz00-sso/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+<meta charset="UTF-8" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+<link type="text/css" rel="stylesheet" href="css/common.css">
+<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
